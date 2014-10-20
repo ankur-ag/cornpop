bd0b334ce373d74b6ba80d2ab379311bc9a7b6ba
vagrant <vagrant@ubuntu-12.04-omnibus-chef>
2014-10-20 14:09:50 +0000

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cornpop::Application.config.secret_key_base = '9c19af3e6a08c6bd9732ef07b68fa64987946a41198d16dbc2d2a5b75993a228586d8508ab419f3a0917793a55db5e64e63d30bcc677a8dfa701487c18af2adb'
